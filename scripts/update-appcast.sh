ca2e488de3f6b435d2a7c64d090c1add4da8e8f5
Kieran Klukas <me@dunkirk.sh>
2026-03-19 09:54:52 +0000

    bug: fix appcast gen

--- scripts/update-appcast.sh
+++ scripts/update-appcast.sh
@@ -44,15 +44,7 @@ if in_list: lines.append('</ul>')
 print('\n'.join(lines))
 ")
 
-cat > docs/appcast.xml << APPCAST_EOF
-<?xml version="1.0" encoding="utf-8"?>
-<rss version="2.0" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle" xmlns:dc="http://purl.org/dc/elements/1.1/">
-  <channel>
-    <title>FRC Mac DS Updates</title>
-    <link>https://taciturnaxolotl.github.io/frc-macos-ds/appcast.xml</link>
-    <description>Most recent updates to FRC Mac DS</description>
-    <language>en</language>
-    <item>
+NEW_ITEM="    <item>
       <title>Version ${VERSION}</title>
       <pubDate>${PUBDATE}</pubDate>
       <sparkle:version>${VERSION}</sparkle:version>
@@ -60,14 +52,42 @@ cat > docs/appcast.xml << APPCAST_EOF
       <sparkle:minimumSystemVersion>14.0</sparkle:minimumSystemVersion>
       <description><![CDATA[${RELEASE_NOTES_HTML}]]></description>
       <enclosure
-        url="${DMG_URL}"
-        sparkle:edSignature="${SPARKLE_SIGNATURE}"
-        length="${DMG_SIZE}"
-        type="application/octet-stream"
-        sparkle:os="macos"/>
-    </item>
+        url=\"${DMG_URL}\"
+        sparkle:edSignature=\"${SPARKLE_SIGNATURE}\"
+        length=\"${DMG_SIZE}\"
+        type=\"application/octet-stream\"
+        sparkle:os=\"macos\"/>
+    </item>"
+
+APPCAST_FILE="docs/appcast.xml"
+
+if [ -f "$APPCAST_FILE" ]; then
+  # Insert the new item after <language>en</language>, before existing items
+  python3 << PYEOF
+appcast = open("$APPCAST_FILE").read()
+marker = "<language>en</language>"
+idx = appcast.find(marker)
+if idx == -1:
+    raise SystemExit("Error: could not find <language> tag in appcast.xml")
+end = idx + len(marker)
+new_item = """
+$NEW_ITEM"""
+result = appcast[:end] + new_item + appcast[end:]
+open("$APPCAST_FILE", "w").write(result)
+PYEOF
+else
+  cat > "$APPCAST_FILE" << APPCAST_EOF
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle" xmlns:dc="http://purl.org/dc/elements/1.1/">
+  <channel>
+    <title>FRC Mac DS Updates</title>
+    <link>https://taciturnaxolotl.github.io/frc-macos-ds/appcast.xml</link>
+    <description>Most recent updates to FRC Mac DS</description>
+    <language>en</language>
+${NEW_ITEM}
   </channel>
 </rss>
 APPCAST_EOF
+fi
 
 echo "Appcast updated for version ${VERSION}"
